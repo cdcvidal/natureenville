cebfd762a5590248c3f293b8d584bb64837d79d1
cdcvidal <celine_vidal@natural-solutions.eu>
2015-07-21 16:07:18 +0200

    Add defaults attributes

--- www/js/models/poi.js
+++ www/js/models/poi.js
@@ -5,10 +5,45 @@ Backbone.LocalStorage = require("backbone.localstorage");
 
 Poi = Backbone.Model.extend({
     defaults: function() {
+        var self = this;
       return {
-        title: "empty todo...",
-        order: Todos.nextOrder(),
-        done: false
+        externalId: 0,
+        longitude: 0,
+        latitude: 0,
+        name:'',
+        name_en:'',
+        name_fr:'',
+        name_es:'',
+        name_de:'',
+        name_it:'',
+        visit_time_min: 0 ,
+        visit_time_max: 0,
+        price_min: '',
+        price_max: '',
+        type_id: '',
+        street: '',
+        postal_code:'',
+        phone:'',
+        mail:'',
+        website:'',
+        resa_link:'',
+        url_img1:'',
+        url_img2:'',
+        desc:'',
+        desc_en:'',
+        desc_fr:'',
+        desc_es:'',
+        desc_de:'',
+        desc_it:'',
+        withchild: true,
+        period:{    poi_id: self.get(externalId),
+                    interval: [1,1,1,1,1,1,1],
+                    open_hour: 0,
+                    close_hour: 0,
+                    open_date: 21/07/2015,
+                    close_date: 21/07/2015
+                }
+
       };
     },
 
