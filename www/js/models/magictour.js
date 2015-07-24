cc5cdc847fcee82dd2440dbfca2404331962f641
cdcvidal <celine_vidal@natural-solutions.eu>
2015-07-24 17:25:59 +0200

    Update attributes and parse method

--- www/js/models/magictour.js
+++ www/js/models/magictour.js
@@ -1,38 +1,34 @@
+'use strict';
+
 var Backbone = require('backbone'),
     $ = require('jquery');
 
+
 Backbone.LocalStorage = require("backbone.localstorage");
 
-MagicTour = Backbone.Model.extend({
+var MagicTour = Backbone.Model.extend({
     defaults: function() {
         return {
-            user_id: "", // result ws login
-            arr_x:5.437166, //longitude pt arrivéee
-            arr_y:43.2291625, //latitude pt arrivée
-            etype_einflu:{5:3,4:3,1:2,3:2,2:0,6:0}, //podération des différents types
-            dep_x:5.437166, //longitude pt départ
-            dep_y:43.2291625, //latitude pt départ
-            language:'fr',
-            option_date:'21/07/2015',
-            option_heure:540, //heure du départ en minute
-            option_temps:120, //durée en minutes
-            option_days:1, //nb de jour
-            option_distance:5000,
-            option_with_child:true //avec ou sans enfant
+            nb_stops: "2",
+            stops: [],
+            t_length: "0",
+            time: "0",
+            trip: []
       };
     },
 
     initialize: function () {
 
     },
+
     url: 'http://dev.optitour.fr/magic/naturalsolution/magictour/',
 
     parse: function(response, options){
-        console.log(response);
+        return response.tours[0];
     }
 
 });
-MagicTourCollection = Backbone.Collection.extend({
+var MagicTourCollection = Backbone.Collection.extend({
 
     model: MagicTour,
 	url: 'http://dev.optitour.fr/magic/naturalsolution/magictour/',
