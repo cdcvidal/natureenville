76bc0a457cd85b3aad4279175ae36adb52c7dcf8
Gilles Bassière <gilles_bassiere@natural-solutions.eu>
2015-08-04 12:25:08 +0200

    [#99782508] show trip on the map

--- www/js/boucleCarte/boucleCarte.js
+++ www/js/boucleCarte/boucleCarte.js
@@ -12,6 +12,8 @@ var baseview = require('../baseview'),
  * OpenLayers map configuration
  */
 var view = new ol.View(), // Map visible area (parameters will be set during view rendering)
+    tripSource = new ol.source.Vector(), // Datasource for trips
+    wktParser = new ol.format.WKT(), // WKT parser will decode geometries from the 
     map = new ol.Map({
         layers: [
             // Layer 1: Basemap
@@ -25,6 +27,16 @@ var view = new ol.View(), // Map visible area (parameters will be set during vie
                     maxZoom: 19,
                     url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 })
+            }),
+            // Layer 2: Trips
+            new ol.layer.Vector({
+                source: tripSource,
+                style: new ol.style.Style({
+                    stroke: new ol.style.Stroke({
+                        color: '#46b992',
+                        width: 3
+                    })
+                })
             })
         ],
         view: view
@@ -36,12 +48,40 @@ var view = new ol.View(), // Map visible area (parameters will be set during vie
 var boucleCarteView = baseview.extend({
         template: require('./boucleCarte.html'),
 
+        displayTrips: function() {
+            // Clear vector layer beforehand
+            tripSource.clear();
+
+            var features,
+                trips = this.model.get('trip');
+
+            if (trips) {
+                // Iterate over trip segments and prepare them for OL display
+                features = trips.map(function (trip, idx, arr) {
+
+                    // Parse a segment and build an OL feature
+                    var feature = wktParser.readFeature(trip.geom);
+
+                    // Convert GPX/WGS84 coordinates to Spherical Mercator (which is the basemap projection)
+                    feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
+
+                    return feature;
+                });
+                // Add features to the vector layer
+                tripSource.addFeatures(features);
+            }
+        },
+
         afterRender: function () {
             // center on current position only at first render, otherwiser reload map at previous location
             if (! view.getCenter()) {
                 view.setZoom(15);
                 view.setCenter(ol.proj.transform([currentPos.get('longitude'), currentPos.get('latitude')], 'EPSG:4326', 'EPSG:3857'));
             }
+
+            // show trips
+            this.displayTrips();
+
             // Attach map to the DOM (to the #map element)
             map.setTarget('map');
         },
