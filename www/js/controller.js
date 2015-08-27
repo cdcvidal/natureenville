b355a774ccf2060e5d8f9d5134159d2cd34d519d
Gilles Bassière <gilles_bassiere@natural-solutions.eu>
2015-08-27 11:20:14 +0200

    Consistent use of controller object (even if I don't really like this pattern...)

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var HomeView = require('./home/home'),
-    $ = require('jquery'),
+var $ = require('jquery'),
+
+    currentPos = require('./current-position'),
     poi = require('./models/poi'),
+    user = require('./models/user'),
+    badges = require('./models/badge').instanceColl,
+    magicTour = require('./models/magictour'),
+
+    HomeView = require('./home/home'),
+    ProfileView = require('./profile/profile'),
+    BoucleDetailView = require('./boucleDetail/boucleDetail'),
+    BoucleCarteView = require('./boucleCarte/boucleCarte'),
+    FichePoiView = require('./fichePoi/fichePoi'),
     ContributionView = require('./contribution/contribution');
 
 /*
@@ -17,17 +27,73 @@ var Controller = function() {
 // "Action" methods
 Controller.prototype.homeViewDisplay = function () {
     $('body').alterClass('section-*', 'section-home');
-    var homeV = new HomeView();
-    this._displayView(homeV);
+    this._displayView(new HomeView());
+};
+
+Controller.prototype.profileViewDisplay = function() {
+    $('body').alterClass('section-*', 'section-loop section-profile');
+    var v = new ProfileView({
+        model: user,
+        collection: badges
+    });
+    this._displayView(v);
+};
+
+Controller.prototype.boucleDetailViewDisplay = function() {
+    $('body').alterClass('section-*', 'section-loop section-loop-details');
+    var v = new BoucleDetailView({
+        model: magicTour
+    });
+    this._displayView(v);
+};
+
+Controller.prototype.boucleCarteViewDisplay = function() {
+    $('body').alterClass('section-*', 'section-loop section-loop-map');
+
+    // Load a tour with default values
+    currentPos.promise().done(function() {
+        var lat = currentPos.get('latitude'),
+            lon = currentPos.get('longitude');
+        magicTour.request.set({
+            dep_x: lon,
+            dep_y: lat,
+            arr_x: lon,
+            arr_y: lat
+        });
+        magicTour.fetch();
+    });
+
+    var v = new BoucleCarteView({
+        model: magicTour
+    });
+    this._displayView(v);
+};
+
+Controller.prototype.ficheViewDisplay = function(poiId) {
+    $('body').alterClass('section-*', 'section-poi');
+    var currentPOI = magicTour.attributes.stops.filter(function(item) {
+        return item.poi_id == poiId;
+    })[0];
+
+    var poiM = new poi.Poi({
+        'externalId': currentPOI.poi_id,
+        'desc_fr': currentPOI.desc,
+        'url_img1': currentPOI.image,
+        'name_fr': currentPOI.place_name
+    });
+    var v = new FichePoiView({
+        model: poiM
+    });
+    this._displayView(v);
 };
 
 Controller.prototype.contributionViewDisplay = function () {
     $('body').alterClass('section-*', 'section-contribution');
     var poiM = new poi.Poi();
-    var contributionV = new ContributionView({
+    var v = new ContributionView({
         model: poiM
     });
-    this._displayView(contributionV);
+    this._displayView(v);
 };
 
 // Helper method for switching main view
--- www/js/router.js
+++ www/js/router.js
@@ -1,30 +1,15 @@
 'use strict';
 
-var Backbone = require('backbone');
-var $ = require('jquery');
-var _ = require('lodash'),
-    currentPos = require('./current-position');
+var Backbone = require('backbone'),
+    controller = require('./controller');
 
-var poi = require('./models/poi');
-var magicTour = require('./models/magictour');
-var user = require('./models/user');
-var controller = require('./controller');
-
-
-var badgesInstanceColl = require('./models/badge').instanceColl;
-
-
-var ProfileView = require('./profile/profile'),
-    BoucleDetailView = require('./boucleDetail/boucleDetail'),
-    BoucleCarteView = require('./boucleCarte/boucleCarte'),
-    FichePoiView = require('./fichePoi/fichePoi'),
-    Router = Backbone.Router.extend({
+var Router = Backbone.Router.extend({
         routes: {
-            '': "homeViewDisplay",
-            'profile': "profileViewDisplay",
-            'loop/map': "boucleCarteViewDisplay",
-            'loop/details': "boucleDetailViewDisplay",
-            'fiche/:poiId': "ficheViewDisplay",
+            '': 'homeViewDisplay',
+            'profile': 'profileViewDisplay',
+            'loop/map': 'boucleCarteViewDisplay',
+            'loop/details': 'boucleDetailViewDisplay',
+            'fiche/:poiId': 'ficheViewDisplay',
             'contribution': 'contributionViewDisplay'
         },
 
@@ -32,80 +17,25 @@ var ProfileView = require('./profile/profile'),
             controller.homeViewDisplay();
         },
 
+        profileViewDisplay: function() {
+            controller.profileViewDisplay();
+        },
+
+        boucleDetailViewDisplay: function() {
+            controller.boucleDetailViewDisplay();
+        },
+
+        boucleCarteViewDisplay: function() {
+            controller.boucleCarteViewDisplay();
+        },
+
         contributionViewDisplay: function() {
             controller.contributionViewDisplay();
         },
 
-        profileViewDisplay: function() {
-            $('body').alterClass('section-*', 'section-loop section-profile');
-            var profileV = new ProfileView({
-                    model: user,
-                    collection: badgesInstanceColl
-                });
-            this.displayView(profileV);
-        },
-
-        boucleDetailViewDisplay: function() {
-            $('body').alterClass('section-*', 'section-loop section-loop-details');
-            var boucleDetailV = new BoucleDetailView({
-                model: magicTour
-            });
-            this.displayView(boucleDetailV);
-        },
-        boucleCarteViewDisplay: function() {
-            $('body').alterClass('section-*', 'section-loop section-loop-map');
-
-            // Load a tour with default values
-            currentPos.promise().done(function() {
-                var lat = currentPos.get('latitude'),
-                    lon = currentPos.get('longitude');
-                magicTour.request.set({
-                    dep_x: lon,
-                    dep_y: lat,
-                    arr_x: lon,
-                    arr_y: lat
-                });
-                magicTour.fetch();
-            });
-
-            var boucleCarteV = new BoucleCarteView({
-                model: magicTour
-            });
-            this.displayView(boucleCarteV);
-        },
-        ficheViewDisplay: function(poiId) {
-            $('body').alterClass('section-*', 'section-poi');
-            var currentPOI = _.find(magicTour.attributes.stops, function(item) {
-                return item.poi_id == poiId;
-            });
-
-            var poiM = new poi.Poi();
-            poiM.set({
-            	'externalId': currentPOI.poi_id,
-            	'desc_fr': currentPOI.desc,
-            	'url_img1': currentPOI.image,
-            	'name_fr': currentPOI.place_name
-            });
-            var ficheV = new FichePoiView({
-                model: poiM
-            });
-            this.displayView(ficheV);
-        },
-
-        _currentView: null,
-
-        displayView: function(view) {
-            if (this._currentView) {
-                this._currentView.remove();
-                this._currentView.off();
-            }
-            this._currentView = view;
-            $('#main').empty();
-            $('#main').append(view.el);
-            view.render();
-        },
+        ficheViewDisplay: function() {
+            controller.ficheViewDisplay();
+        }
     });
 
-var router = new Router();
-
-module.exports = router;
+module.exports = new Router();
