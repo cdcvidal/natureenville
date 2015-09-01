93d78e76edba717b87fbcb0658bc334a4c33e561
Gilles Bassière <gilles_bassiere@natural-solutions.eu>
2015-09-01 15:24:46 +0200

    fichePoi: code-clean up (remove commented or unused code, fix template indentation) + adapt to new poi model

--- www/js/fichePoi/fichePoi.js
+++ www/js/fichePoi/fichePoi.js
@@ -1,46 +1,25 @@
 'use strict';
 
-var BaseView = require('../baseview');
-var $ = require('jquery');
-var _ = require('lodash');
-var moment = require('moment');
-//var Swiper = require('swiper');
+var BaseView = require('../baseview'),
+    _ = require('lodash'),
+    moment = require('moment');
 
 var FichePoiView = BaseView.extend({
     template: require('./fichePoi.html'),
     sectionClass: 'section-poi',
 
-    initialize: function () {
-        //moment.locale('fr');
-        BaseView.prototype.initialize.apply(this, arguments);
-    },
     serialize: function () {
-        var self = this;
         var openingDays = [];
-        _.forEach(self.model.get('period').get('interval'), function(isOpen, index) {
+        _.forEach(this.model.get('period').get('interval'), function(isOpen, index) {
             if ( isOpen )
                 openingDays.push(_.capitalize(moment().day(index).format('ddd')));
         });
-        //console.log(_.isArray(openingDays.split));
         return {
-            model: self.model,
+            open_hour: this.model.get('period').get('open_hour'),
+            address: this.model.get('street'),
+            desc: this.model.get('desc_fr'),
             openingDays: openingDays.join(' ')
         };
-    },
-    afterRender: function(){
-        /*this.swiper = new Swiper('.swiper-container', {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            loop: false,
-            pagination: $('.swiper-pagination'),
-            paginationClickable: true,
-            paginationBulletRender: function (index, className) {
-                return '<li class="'+ className +'"></li>';
-            }
-        });*/
-    },
-    remove: function() {
-        BaseView.prototype.remove.apply(this, arguments);
     }
 });
 
