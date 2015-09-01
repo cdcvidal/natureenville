1c31a457aef45370ddad89a57c51390485ae123b
cdcvidal <celine_vidal@natural-solutions.eu>
2015-09-01 12:53:33 +0200

    Add method cordova checkconnection

--- www/js/utilities.js
+++ www/js/utilities.js
@@ -5,5 +5,21 @@ var moment = require('moment');
 module.exports = {
     formatMinutes: function(minutes) {
         return moment().startOf('day').add(parseFloat(minutes), 'minutes').format('h:m');
+    },
+    checkConnection: function() {
+        if (navigator.connection) {
+            var networkState = navigator.connection.type;
+            var states = {};
+            var Connection;
+            states[Connection.UNKNOWN] = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI] = 'WiFi connection';
+            states[Connection.CELL_2G] = 'Cell 2G connection';
+            states[Connection.CELL_3G] = 'Cell 3G connection';
+            states[Connection.CELL_4G] = 'Cell 4G connection';
+            states[Connection.CELL] = 'Cell generic connection';
+            states[Connection.NONE] = 'No network connection';
+            return states[networkState];
+        }
     }
 };
