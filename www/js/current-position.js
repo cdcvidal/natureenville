16d725c54dcce67cfa39848cfa2d71a0c7630dcf
Vincent BOURGEOIS <vincent@Mac-mini-de-Vincent.local>
2015-09-07 11:22:37 +0200

    Increase geoloc timeout

--- www/js/current-position.js
+++ www/js/current-position.js
@@ -12,7 +12,7 @@ var GeoModel = Backbone.Model.extend({
     defaultOptions: {
         enableHighAccuracy: false,
         maximumAge: 10000,
-        timeout: 5000
+        timeout: 10000
     },
 
     initialize: function(options) {
